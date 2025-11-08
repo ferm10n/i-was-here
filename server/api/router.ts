41530498a8b54f08dc2bff81ecac90543889e594
John Sanders <jws121295@gmail.com>
2025-11-07 21:54:17 -0500

    add introspection endpoint to router

--- server/api/router.ts
+++ server/api/router.ts
@@ -1,8 +1,10 @@
 import { WideApiEndpointDef } from './util.ts';
 import { helloEndpoint } from './routes/hello.ts';
+import { introspectionEndpoint } from './routes/introspection.ts';
 
 export const router = {
   '/api/hello': helloEndpoint,
+  '/api/introspection': introspectionEndpoint,
 } satisfies {
   [path: string]: WideApiEndpointDef;
 };
