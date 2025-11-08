f5d931fb3f7e51672a3f428d3199e9c14e964c31
John Sanders <jws121295@gmail.com>
2025-11-07 22:38:55 -0500

    dynamic favicon

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,15 @@ import App from './App.vue'
 // Composables
 import { createApp } from 'vue'
 
+// Assets
+import faviconUrl from './assets/favicon.ico'
+
+// Set favicon with content hash so it works caching well
+const link = document.querySelector("link[rel='icon']") as HTMLLinkElement
+if (link) {
+  link.href = faviconUrl
+}
+
 const app = createApp(App)
 
 registerPlugins(app)
