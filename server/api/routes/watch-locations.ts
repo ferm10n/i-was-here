fd9266aacc163d62c25a1f97a7ffe06d8e478e60
John Sanders <jsanders@redviking.com>
2025-11-18 21:58:16 -0500

    fix unsubscribe handling

--- server/api/routes/watch-locations.ts
+++ server/api/routes/watch-locations.ts
@@ -6,12 +6,12 @@ export const watchLocationsEndpoint = defineEndpoint({
   inputSchema: null,
   protected: true,
   handler: (_reqBody, _req, _user) => {
-    let unsubscribe: (() => void) | null;
+    let unsubscribe: (() => void) | null = null;
 
     const body = new ReadableStream({
       start(controller) {
         controller.enqueue(textEncoder.encode(`event: started\n\n`))
-        unsubscribe = locations$.subscribe(newLocation => {
+        const subscriber = locations$.subscribe(newLocation => {
           try {
             const msg = textEncoder.encode(`data: ${JSON.stringify({ id: newLocation.id, lat: newLocation.lat, lng: newLocation.lng })}\n\n`);
             controller.enqueue(msg);
@@ -20,7 +20,10 @@ export const watchLocationsEndpoint = defineEndpoint({
             console.log('Client may have disconnected from location stream', error);
             if (unsubscribe) unsubscribe();
           }
-        }).unsubscribe;
+        });
+        unsubscribe = () => {
+          subscriber.unsubscribe();
+        }
       },
       cancel() {
         if (unsubscribe) unsubscribe();
